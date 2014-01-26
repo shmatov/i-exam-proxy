ce9365aab35e3581756372ec5d131785bec15431
Roman Shmatov <romanshmatov@gmail.com>
2014-01-26 16:18:19 +0600

    REMO-O-O-OVE!

--- replace/static/js/test.js
+++ replace/static/js/test.js
@@ -769,66 +769,27 @@ function initScreenLoader() {
 
 // запрет на выделение текста
 function disableSelection(element) {
-    var isPreventSelection = false;
-
     function addHandler(element, event, handler) {
-        if (element.attachEvent) {
-            element.attachEvent('on' + event, handler);
-        } else {
-            if (element.addEventListener) {
-                element.addEventListener(event, handler, false);
-            }
-        }
     }
 
     function removeSelection() {
-        if (window.getSelection) {
-            window.getSelection().removeAllRanges();
-        } else if (document.selection && document.selection.clear) {
-            document.selection.clear();
-        }
+      return;
     }
 
     function killCtrlA(event) {
-        var event = event || window.event;
-        var sender = event.target || event.srcElement;
-
-        if (sender.tagName.match(/INPUT|TEXTAREA/i)) {
-            return;
-        }
-
-        var key = event.keyCode || event.which;
-        if (event.ctrlKey && key == 'A'.charCodeAt(0)) { // 'A'.charCodeAt(0) можно заменить на 65
-            removeSelection();
-
-            if (event.preventDefault) {
-                event.preventDefault();
-            } else {
-                event.returnValue = false;
-            }
-        }
+      return;
     }
 
     // не даем выделять текст мышкой
     addHandler(element, 'mousemove', function () {
-        if (isPreventSelection) {
-            removeSelection();
-        }
     });
     addHandler(element, 'mousedown', function (event) {
-        var event = event || window.event;
-        var sender = event.target || event.srcElement;
-        isPreventSelection = !sender.tagName.match(/INPUT|TEXTAREA/i);
     });
 
     // борем dblclick
     // если вешать функцию не на событие dblclick, можно избежать
     // временное выделение текста в некоторых браузерах
     addHandler(element, 'mouseup', function () {
-        if (isPreventSelection) {
-            removeSelection();
-        }
-        isPreventSelection = false;
     });
 
     // борем ctrl+A
